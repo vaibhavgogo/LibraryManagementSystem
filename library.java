7f5e740ddc215cf7e07a65dc291a2b4811b905f7
vaibhavgogo <vaibhavsmartkid@gmail.com>
2025-09-11 02:54:47 +0530

    commit

--- library.java
+++ library.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package LibraryManagementSystem;
 
 import java.util.*;
@@ -9,108 +9,87 @@ public class library {
 
         // Displaying menu
         System.out.println(
-                "********************Welcome to the GFG Library!********************");
+                "********************Welcome to the Library!********************");
         System.out.println(
                 "                  Select From The Following Options:               ");
         System.out.println(
                 "**********************************************************************");
 
-        // Creating object of book class
+
         books ob = new books();
-        // Creating object of students class
+
         students obStudent = new students();
-
-        int choice;
-        int searchChoice;
-
-        // Creating menu
-        // using do-while loop
-        do {
-
-            ob.dispMenu();
-            choice = input.nextInt();
-
-            // Switch case
-            switch (choice) {
-
-                // Case
-                case 1:
-                    book b = new book();
-                    ob.addBook(b);
-                    break;
-
-                // Case
-                case 2:
-                    ob.upgradeBookQty();
-                    break;
-
-                // Case
-                case 3:
-
-                    System.out.println(
-                            " press 1 to Search with Book Serial No.");
-                    System.out.println(
-                            " Press 2 to Search with Book's Author Name.");
-                    searchChoice = input.nextInt();
-
-                    // Nested switch
-                    switch (searchChoice) {
-
-                        // Case
-                        case 1:
-                            ob.searchBySno();
-                            break;
-
-                        // Case
-                        case 2:
-                            ob.searchByAuthorName();
-                    }
-                    break;
-
-                // Case
-                case 4:
-                    ob.showAllBooks();
-                    break;
-
-                // Case
-                case 5:
-                    student s = new student();
-                    obStudent.addStudent(s);
-                    break;
-
-                // Case
-                case 6:
-                    obStudent.showAllStudents();
-                    break;
-
-                // Case
-                case 7:
-                    obStudent.checkOutBook(ob);
-                    break;
-
-                // Case
-                case 8:
-                    obStudent.checkInBook(ob);
-                    break;
-
-                // Default case that will execute for sure
-                // if above cases does not match
-                default:
-
-                    // Print statement
-                    System.out.println("ENTER BETWEEN 0 TO 8.");
-            }
-
-        }
-
-        // Checking condition at last where we are
-        // checking case entered value is not zero
-        while (choice != 0);
+//
+//        int choice;
+//        int searchChoice;
+//
+//
+//        do {
+//
+//            ob.dispMenu();
+//            choice = input.nextInt();
+//
+//
+//            switch (choice) {
+//
+//
+//                case 1:
+//                    book b = new book();
+//                    ob.addBook(b);
+//                    break;
+//
+//                case 2:
+//                    ob.upgradeBookQty();
+//                    break;
+//
+//                case 3:
+//
+//                    System.out.println(
+//                            " press 1 to Search with Book Serial No.");
+//                    System.out.println(
+//                            " Press 2 to Search with Book's Author Name.");
+//                    searchChoice = input.nextInt();
+//
+//                    switch (searchChoice) {
+//
+//                        case 1:
+//                            ob.searchBySno();
+//                            break;
+//
+//                        case 2:
+//                            ob.searchByAuthorName();
+//                    }
+//                    break;
+//
+//                case 4:
+//                    ob.showAllBooks();
+//                    break;
+//
+//                case 5:
+//                    student s = new student();
+//                    obStudent.addStudent(s);
+//                    break;
+//
+//                case 6:
+//                    obStudent.showAllStudents();
+//                    break;
+//
+//                case 7:
+//                    obStudent.checkOutBook(ob);
+//                    break;
+//
+//                case 8:
+//                    obStudent.checkInBook(ob);
+//                    break;
+//
+//                default:
+//
+//                    System.out.println("ENTER BETWEEN 0 TO 8.");
+//            }
+//
+//        }
+//
+//
+//        while (choice != 0);
     }}
-=======
-package LibraryManagementSystem;
 
-public class library {
-    
-}
->>>>>>> 90a7be533b944df557f534a54d901fede8e5b761
---
+++ students.java
@@ -0,0 +1,4 @@
+package LibraryManagementSystem;
+
+public class students {
+}
